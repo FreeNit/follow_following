ba9dab0e1a4a42ee10333a48092f9e4c12bd4127
Dimon <bolilui.dmytro@gmail.com>
2023-05-21 15:20:25 +0300

    ADDED: spinner during the data is loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { GlobalStyle } from './components/GlobalStyle';
 import { getUsers } from './redux/operations';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 
 import { UsersCollection } from './components/users/UsersList';
 
-import { AppStyled } from './components/App/App.styled';
+import { AppStyled, SpinnerPopup } from './components/App/App.styled';
+import { selectIsLoading } from './redux/selectors';
 
 function App() {
   const dispatch = useDispatch();
@@ -14,8 +15,16 @@ function App() {
     dispatch(getUsers());
   }, [dispatch]);
 
+  const isLoadingValue = useSelector(selectIsLoading);
+
   return (
     <AppStyled>
+      {isLoadingValue && (
+        <SpinnerPopup>
+          <img src={require('./images/spinner.gif')} alt='spinner' />
+          <h3>...Data is loading</h3>
+        </SpinnerPopup>
+      )}
       <section>
         <UsersCollection />
       </section>
--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -4,3 +4,21 @@ export const AppStyled = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const SpinnerPopup = styled.div`
+  position: fixed;
+  z-index: 100;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(221, 221, 221, 0.85);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  img {
+    width: 300px;
+    height: 300px;
+    object-fit: fill;
+  }
+`;
